fae4504154f9eb03a5361216efb269ff14cd9ba1
ArtemKomarec <artemkomarec10@gmail.com>
2022-11-29 13:43:40 +0300

    test jokes

--- src/services/joke.js
+++ src/services/joke.js
@@ -1,20 +1,24 @@
-const axios = require('axios');
+const axios = require("axios");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-const constants = require('../constants');
+const constants = require("../constants");
 
 const getRandomJoke = async () => {
-    const res = await axios.get(constants.JOKE_API_RUS);
-    const { window: { document } } = new JSDOM(res.data);
+	const res = await axios.get(constants.JOKE_API_RUS);
+	const {
+		window: { document },
+	} = new JSDOM(res.data);
 
-    const jokesList = document.querySelectorAll('.topicbox[data-id] div.text');
+	const jokesList = document.querySelectorAll(".topicbox[data-id] div.text");
 
-    const randomJokeIndex = Math.floor(Math.random() * jokesList.length - 1);
-    if(jokesList[randomJokeIndex]) {
-        return jokesList[randomJokeIndex].innerHTML
-    } else{
-        'Анекдотов нет, расходимся'
-    }
-}
+	const randomJokeIndex = Math.floor(Math.random() * jokesList.length - 1);
+	console.log(jokesList[randomJokeIndex].innerHTML);
+
+	if (jokesList[randomJokeIndex]) {
+		return jokesList[randomJokeIndex].innerHTML;
+	} else {
+		return "Анекдотов нет, расходимся";
+	}
+};
 
 module.exports.getRandomJoke = getRandomJoke;
